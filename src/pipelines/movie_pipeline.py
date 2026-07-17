b5e3d692eb56d9cd4e5f99f4d9867c813c8045ed
Smit Nikumbe <nikumbesmit0306@gmail.com>
2026-07-17 17:30:55 +0530

    aggregate ratings, tags, and merge movie datasets

--- src/pipelines/movie_pipeline.py
+++ src/pipelines/movie_pipeline.py
@@ -6,7 +6,7 @@ from src.data.constants import RAW_DATA_DIR
 class MoviePipeline :
 
     """Pipeline for Unified Movie Dataset Building """
-    
+
     def load_datasets(self) :
         
         movies = load_csv(RAW_DATA_DIR / "movielens" / "movies.csv")
@@ -14,16 +14,61 @@ class MoviePipeline :
         tags = load_csv(RAW_DATA_DIR / "movielens" / "tags.csv")
         links = load_csv(RAW_DATA_DIR / "movielens" / "links.csv")
 
-        return movies, ratings, links , tags
+        return {
+            "movies" : movies,
+            "ratings" : ratings,
+            "links" : links,
+            "tags" : tags,
 
-    def aggregate_ratings(self) :
-        pass
+        }
 
-    def aggregate_tags(self) :
-        pass
+    def aggregate_ratings(self,ratings) :
+        
+        rating_summary = (
+            ratings.groupby("movieId",as_index= False)
+            .agg(
+                average_rating = ("rating","mean"),
+                rating_count = ("rating","count"),
+            )
+        )
 
-    def merge_datasets(self) :
-        pass
+        rating_summary["average_rating"] = (rating_summary["average_rating"].round(2))
+
+        return rating_summary
+
+    def aggregate_tags(self,tags) :
+        
+        tag_summary = (
+            tags.groupby("movieId",as_index = False)
+            .agg(
+            tags = ("tag", lambda x : " ".join(x.dropna().astype(str)))
+            )
+        )
+        
+        return tag_summary
+        
+
+    def merge_datasets(self,movies,rating_summary,links,tag_summary) :
+        
+        movie_features = movies.merge(
+            rating_summary,
+            on = "movieId",
+            how = "left"
+        )
+
+        movie_features = movie_features.merge(
+            tag_summary,
+            on = "movieId",
+            how = "left"
+        )
+
+        movie_features = movie_features.merge(
+            links,
+            on = "movieId",
+            how = "left"
+        )
+
+        return movie_features
     
     def save_processed_dataset(self) :
         pass 
