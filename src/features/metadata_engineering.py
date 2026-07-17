c435404234a799ab0db671aabaca84270a4eb3fd
Smit Nikumbe <nikumbesmit0306@gmail.com>
2026-07-17 17:31:08 +0530

    refactor(pipeline): separate metadata feature engineering

---
+++ src/features/metadata_engineering.py
@@ -0,0 +1,15 @@
+class MetadataFeatureEngineer :
+
+    """
+    It helps in creating metadata features
+    which will be used by recommendation model
+    """
+
+    def clean_genres(self) :
+        pass
+
+    def clean_tags(self) :
+        pass
+
+    def create_metadata(self) :
+        pass
--- src/pipelines/movie_pipeline.py
+++ src/pipelines/movie_pipeline.py
@@ -70,5 +70,29 @@ class MoviePipeline :
 
         return movie_features
     
+    def create_metadata(self,movie_features) :
+
+        movie_features["tags"] = movie_features["tags"].fillna("") 
+
+        movie_features["genres"] = movie_features["genres"].str.replace("|", " ", regex=False)
+
+        movie_features["metadata"] = movie_features["genres"] + " " + movie_features["tags"]
+
+        movie_features["metadata"] = (
+            movie_features["metadata"]
+            .str.lower()
+            .str.replace(r"\s+", " ",regex=True)
+            .str.strip()
+        )
+
+        movie_features["metadata"] = (
+            movie_features["metadata"]
+            .apply(
+                lambda text : " ".join(dict.fromkeys(text.split()))
+            )
+        )
+
+        return movie_features
+
     def save_processed_dataset(self) :
         pass 
